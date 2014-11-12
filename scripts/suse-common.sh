01f628ed66e5cb82a6f845d5da253a823570c024
Manuel Morini <morinimanuel@gmail.com>
2014-11-12 14:48:07 +0100

    fixes

--- scripts/suse-common.sh
+++ scripts/suse-common.sh
@@ -5,6 +5,7 @@ zypper addlock kernel*
 zypper --non-interactive update 
 
 # Tools
-zypper --non-interactive install nano git unzip screen telnet
+zypper --non-interactive remove patterns-openSUSE-minimal_base-conflicts-12.3-7.10.1.x86_64
+zypper --non-interactive install nano git unzip screen telnet smtp_daemon
 
 zypper removelock kernel*
--- scripts/suse-web.sh
+++ scripts/suse-web.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Apache
-sudo zypper --non-interactive install apache2 apache2-mod_perl apache2-mod_php
+zypper install -y apache2 apache2-mod_perl apache2-mod_php5 php5 php5-mbstring php5-pear
+ln -s /usr/bin/perl /usr/local/bin/perl
+
 # chkconfig --add httpd
 # chkconfig httpd on
-# systemctl stop apache2
+ systemctl enable apache2
+ systemctl stop apache2
 
 # Apache main config
 #rm -rf /var/www/html
 #ln -s /vagrant /var/www/html
 
 #service httpd start
-#systemctl start apache2
+ systemctl start apache2
 
-# PHP
-sudo zypper --non-interactive install  php5 php5-cli php5-common php5-devel php5-mysql php5-pear php5-mbstring
 
 # Download Starter Content
 cd /vagrant
 sudo -u vagrant wget -q https://raw.githubusercontent.com/Licas/vagranttest/master/files/index.html
 sudo -u vagrant wget -q https://raw.githubusercontent.com/Licas/vagranttest/master/files/info.php
 
-# systemctl restart apache2
+ systemctl restart apache2
